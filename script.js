d4865d9a34ce65421fc7bb70a800f8d895157c63
sanjana9560 <121418058+sanjana9560@users.noreply.github.com>
2025-07-16 01:22:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function getFormvalue() {
-    // Prevent the default form submission behavior
     event.preventDefault();
 
-    // Get the form element
     const form = document.getElementById("form1");
 
-    // Get and trim values of fname and lname
     const firstName = form.elements["fname"].value.trim();
     const lastName = form.elements["lname"].value.trim();
